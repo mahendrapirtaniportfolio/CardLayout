f1512deb59d6ab1afc531c0ff004ad25d6b79126
Mahendra Pirtani <pirtani.mahendra94@gmail.com>
2023-10-13 15:24:23 +0530

    Code added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,7 +5,7 @@ function Card({imgUrl, title, price}) {
         <div className='overflow-hidden mb-2'>
             <img src={imgUrl}
                 className='hover:scale-125 duration-1000 w-40 h-40 mx-auto py-2'
-            />
+            /> 
     </div>
         <h1 className='text-2xl'>{title}</h1>
         <p className='text-md text-stone-400'>{price} INR/-</p>
